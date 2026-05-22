656b797091a92064c483c10b79238e1de852521d
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-22 23:49:06 +0600

    Pricing Data Updated

---
+++ src/static/Pricing.info.jsx
@@ -0,0 +1,46 @@
+// data
+export const info = {
+  title: "Simple, transparent pricing",
+  description: "Start for free, upgrade when you need more power.",
+  cards: [
+    {
+      plan: "Hobby",
+      cost: 0,
+      duration: "month",
+      features: [
+        "50 links/month",
+        "Basic analytics",
+        "Standard support",
+        "7-day link history",
+      ],
+      action: "Get Started",
+    },
+    {
+      special: "Most Popular",
+      plan: "Pro",
+      cost: 15,
+      duration: "month",
+      features: [
+        "1,000 links/month",
+        "Advanced analytics",
+        "Custom domains",
+        "Dynamic QR codes",
+        "1-year link history",
+      ],
+      action: "Start 7-day Trial",
+    },
+    {
+      plan: "Business",
+      cost: 49,
+      duration: "month",
+      features: [
+        "Unlimited links",
+        "Custom API access",
+        "SSO & Advanced Security",
+        "Team collaboration",
+        "Dedicated success manager",
+      ],
+      action: "Contact Sales",
+    },
+  ],
+};
