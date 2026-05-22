af1980bf09d72e9de1c362eb8cf5bb71a994004a
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-22 23:09:50 +0600

    Structure Update - Index

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 // dependencies
 import { useEffect } from "react";
 // layouts
-import Navbar from "../components/Navbar";
 import Hero from "../layout/Hero.layout";
 import Status from "../layout/Status.layout";
 import Features from "../layout/Features.layout";
 import Pricing from "../layout/Pricing.layout";
 import Footer from "../layout/Footer.layout";
+// components
+import Navbar from "../components/Navbar";
 
 // main
 function Home() {
