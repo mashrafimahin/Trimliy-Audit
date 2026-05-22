83cdeeb2414f57c39b7b46a3ec241e34fb417204
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-22 23:48:47 +0600

    Footer Data Updated

---
+++ src/static/Footer.info.jsx
@@ -0,0 +1,61 @@
+// icon
+
+// data
+export const info = {
+  logo: {
+    bio: "The modern link management platform for forward-thinking teams. Shorten, track, and optimize every touchdowns.",
+  },
+  quickLinks: [
+    {
+      caption: "Product",
+      links: [
+        {
+          itemName: "Link Management",
+          route: "",
+        },
+        {
+          itemName: "QR Codes",
+          route: "",
+        },
+        {
+          itemName: "Link-in-bio",
+          route: "",
+        },
+      ],
+    },
+    {
+      caption: "Resources",
+      links: [
+        {
+          itemName: "Docs",
+          route: "",
+        },
+        {
+          itemName: "API",
+          route: "",
+        },
+        {
+          itemName: "Blog",
+          route: "",
+        },
+      ],
+    },
+    {
+      caption: "Company",
+      links: [
+        {
+          itemName: "About",
+          route: "about",
+        },
+        {
+          itemName: "Privacy",
+          route: "",
+        },
+        {
+          itemName: "Terms",
+          route: "",
+        },
+      ],
+    },
+  ],
+};
