bbce4c7568b3800db45fffa6c8dadf580a615606
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-22 20:09:58 +0600

    Structure Updated : New Route - SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router";
 // pages
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 // main
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Suspense>
     </>
