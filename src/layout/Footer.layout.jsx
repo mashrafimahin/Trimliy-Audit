16386997833318e7b4b07336d39e2a3b4f6aeff8
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-22 23:48:15 +0600

    Footer Layout Updated

--- src/layout/Footer.layout.jsx
+++ src/layout/Footer.layout.jsx
@@ -3,64 +3,7 @@ import { Link } from "react-router";
 // icons
 import { Link2 } from "lucide-react";
 // data
-const data = {
-  logo: {
-    bio: "The modern link management platform for forward-thinking teams. Shorten, track, and optimize every touchdowns.",
-  },
-  quickLinks: [
-    {
-      caption: "Product",
-      links: [
-        {
-          itemName: "Link Management",
-          route: "",
-        },
-        {
-          itemName: "QR Codes",
-          route: "",
-        },
-        {
-          itemName: "Link-in-bio",
-          route: "",
-        },
-      ],
-    },
-    {
-      caption: "Resources",
-      links: [
-        {
-          itemName: "Docs",
-          route: "",
-        },
-        {
-          itemName: "API",
-          route: "",
-        },
-        {
-          itemName: "Blog",
-          route: "",
-        },
-      ],
-    },
-    {
-      caption: "Company",
-      links: [
-        {
-          itemName: "About",
-          route: "about",
-        },
-        {
-          itemName: "Privacy",
-          route: "",
-        },
-        {
-          itemName: "Terms",
-          route: "",
-        },
-      ],
-    },
-  ],
-};
+import { info as data } from "../static/Features.info";
 
 function Footer() {
   return (
