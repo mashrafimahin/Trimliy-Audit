80eb4d72fc922dce4b0d3e9a292a7e76b1a8ccfc
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-22 23:48:55 +0600

    Navigation Data Updated

---
+++ src/static/Navigation.info.jsx
@@ -0,0 +1,21 @@
+// icon
+
+// data
+export const info = [
+  {
+    caption: "Features",
+    route: "features",
+  },
+  {
+    caption: "Analytics",
+    route: "",
+  },
+  {
+    caption: "Pricing",
+    route: "pricing",
+  },
+  {
+    caption: "Docs",
+    route: "",
+  },
+];
