defc2952673b8e403c842ffc5c089672b91ff871
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-22 20:10:06 +0600

    Structure Updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,12 @@
 import { cn } from "../utils/ClassMerger";
 
 // main
-function Button({ variant, className, children, ...rest }) {
+function Button({ variant, type, className, children, ...rest }) {
   return (
     <>
       {variant === "regular" ? (
         <button
+          type={type}
           className={cn(
             "w-full md:w-auto px-8 py-2 shrink-0 flex items-center justify-center bg-gray-900 border border-gray-600 rounded-md cursor-pointer active:scale-[0.96] transition",
             className,
@@ -17,6 +18,7 @@ function Button({ variant, className, children, ...rest }) {
         </button>
       ) : (
         <button
+          type={type}
           className={cn(
             "w-full md:w-auto px-8 py-2 shrink-0 flex items-center justify-center bg-blue-600 rounded-md cursor-pointer active:scale-[0.96] transition",
             className,
