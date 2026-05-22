2b0cbe29cae5df247a83898d8e463b6fd862e593
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-22 11:23:38 +0600

    App Structure Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router";
 
 // pages
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 // main
 function App() {
@@ -13,6 +14,7 @@ function App() {
         {/* routes */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Suspense>
     </>
