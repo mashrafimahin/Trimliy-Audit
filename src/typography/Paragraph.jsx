ca3e017217ec72668bf4912ecf17a4a90b563414
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-23 09:11:16 +0600

    Variants Added

--- src/typography/Paragraph.jsx
+++ src/typography/Paragraph.jsx
@@ -1,10 +1,16 @@
+// dependencies
+import { cn } from "../utils/ClassMerger";
+// variant
+const Variant = {
+  large:
+    "text-lg md:text-xl text-slate-400 mb-10 max-w-2xl mx-auto leading-relaxed",
+  small: "text-slate-300 mb-6 text-lg",
+};
+
 // main
-function Paragraph({ children, ...rest }) {
+function Paragraph({ variant, className, children, ...rest }) {
   return (
-    <p
-      className="text-lg md:text-xl text-slate-400 mb-10 max-w-2xl mx-auto leading-relaxed"
-      {...rest}
-    >
+    <p className={cn(Variant[variant], className)} {...rest}>
       {children}
     </p>
   );
