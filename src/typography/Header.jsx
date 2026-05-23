e12f7696b29d0ef6d632d312c259e6abb777540f
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-23 09:11:07 +0600

    Variant Added

--- src/typography/Header.jsx
+++ src/typography/Header.jsx
@@ -1,23 +1,18 @@
+// dependencies
+import { cn } from "../utils/ClassMerger";
+// variant
+const Variant = {
+  h1: "text-5xl md:text-7xl font-extrabold text-white tracking-tight leading-[1.1] mb-6",
+  h2: "text-3xl md:text-5xl font-bold text-white mb-6",
+  h3: "text-3xl font-bold text-white mb-4",
+};
+
 // main
-function Header({ variant, children, ...rest }) {
+function Header({ variant, className, children, ...rest }) {
   return (
-    <>
-      {variant === "regular" ? (
-        <h2
-          className="text-3xl md:text-5xl font-bold text-white mb-6"
-          {...rest}
-        >
-          {children}
-        </h2>
-      ) : (
-        <h1
-          className="text-5xl md:text-7xl font-extrabold text-white tracking-tight leading-[1.1] mb-6"
-          {...rest}
-        >
-          {children}
-        </h1>
-      )}
-    </>
+    <h1 className={cn(Variant[variant], className)} {...rest}>
+      {children}
+    </h1>
   );
 }
 
