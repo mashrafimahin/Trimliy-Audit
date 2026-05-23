248582c460e51f6075be7d8175a695476b040044
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-23 09:10:34 +0600

    Structure Update : Policy Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const SignUp = lazy(() => import("./pages/SignUp"));
 const About = lazy(() => import("./pages/About"));
+const Privacy = lazy(() => import("./pages/Privacy"));
 
 // main
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/about" element={<About />} />
+          <Route path="/privacy" element={<Privacy />} />
         </Routes>
       </Suspense>
     </>
