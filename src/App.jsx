ab7f7374b057200515681ebdb60c7fffff6e26af
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-24 15:29:14 +0600

    Structure Updated : Auth Checking with Protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // dependencies
 import { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
+// auth handler
+import { useAuth } from "./hooks/useAuth";
 // route handler
 import RouteHandler from "./utils/RouteHandler";
 // loader
@@ -12,21 +14,30 @@ const SignUp = lazy(() => import("./pages/SignUp"));
 const About = lazy(() => import("./pages/About"));
 const Contact = lazy(() => import("./pages/Contact"));
 const Privacy = lazy(() => import("./pages/Privacy"));
+const Dashboard = lazy(() => import("./features/Dashboard"));
 
 // main
 function App() {
+  // auth checkpoint
+  const auth = useAuth();
+
   return (
     <>
       <Suspense fallback={<Loader />}>
         {/* routes */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={auth ? <Navigate to={"/dashboard"} replace /> : <Home />}
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/privacy" element={<Privacy />} />
-          <Route element={<RouteHandler />}></Route>
+          <Route element={<RouteHandler />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
       </Suspense>
     </>
