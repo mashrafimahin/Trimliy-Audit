59a390a63f0e9cf3872435c1d064e2c21913e5ab
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-24 19:36:08 +0600

    Link Layout Updated

---
+++ src/layout/Links.layout.jsx
@@ -0,0 +1,37 @@
+// dependencies
+// typography
+import Header from "../typography/Header";
+import Paragraph from "../typography/Paragraph";
+// components
+import Button from "../components/Button";
+import LinkBox from "../components/LinkBox";
+// icons
+import { Plus } from "lucide-react";
+
+const Links = ({ info }) => {
+  return (
+    <div className="space-y-6">
+      {/* header */}
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
+        <div>
+          <Header variant={"h3"}>My Links</Header>
+          <Paragraph variant={"small"} className={"text-sm -mt-2.5"}>
+            Manage, edit, and track your shortened URLs.
+          </Paragraph>
+        </div>
+        <Button>
+          <Plus className="w-5 h-5 mr-2" /> Create Link
+        </Button>
+      </div>
+
+      {/* link lists */}
+      <div className="space-y-4">
+        {info.map((item, i) => (
+          <LinkBox key={i} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Links;
