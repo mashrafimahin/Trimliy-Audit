39f595be961955f6495295b584f9bd135f20315f
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-25 07:47:49 +0600

    Redux Toolkit Installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import ScrollManager from "./handler/ScrollManager.jsx";
+import { Provider } from "react-redux";
 import "./styles/index.css";
 import App from "./App.jsx";
+import ScrollManager from "./handler/ScrollManager.jsx";
+import Store from "./app/store/Store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <ScrollManager />
-      <App />
-    </BrowserRouter>
+    <Provider store={Store}>
+      <BrowserRouter>
+        <ScrollManager />
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 );
