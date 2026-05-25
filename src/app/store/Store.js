dc55f3d01eac8a2044cf4e683141922829fcfe18
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-25 12:36:55 +0600

    New Data Added : Billing

--- src/app/store/Store.js
+++ src/app/store/Store.js
@@ -2,10 +2,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 // slices
 import ProfileSettingData from "../features/ProfileSettingSlice";
+import BillingSettingData from "../features/BillingSettingSlice";
 
 const Store = configureStore({
   reducer: {
     profileData: ProfileSettingData,
+    billingData: BillingSettingData,
     dashboardPopup: null,
   },
 });
