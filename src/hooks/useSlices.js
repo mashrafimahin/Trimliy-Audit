a187e2b2b0a80c4c0f8794de360b5bbc34ccb8d1
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-25 12:36:18 +0600

    Export Name changed

--- src/hooks/useSlices.js
+++ src/hooks/useSlices.js
@@ -2,7 +2,7 @@
 import { useSelector, useDispatch } from "react-redux";
 
 // data
-export function useProfileData(path) {
+export function useSlices(path) {
   const data = useSelector((state) => state[path]);
   const dispatch = useDispatch();
   return { data, dispatch };
