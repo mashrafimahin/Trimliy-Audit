a15f5158bedb31b3c78e021e56c359b5d132df50
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-25 12:53:54 +0600

    New Slice Added : Links

--- src/app/store/Store.js
+++ src/app/store/Store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 // slices
 import ProfileSettingData from "../features/ProfileSettingSlice";
 import BillingSettingData from "../features/BillingSettingSlice";
+import LinksSettingSlice from "../features/LinksSettingSlice";
 
 const Store = configureStore({
   reducer: {
     profileData: ProfileSettingData,
     billingData: BillingSettingData,
+    linksData: LinksSettingSlice,
     dashboardPopup: null,
   },
 });
