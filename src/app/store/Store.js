4b1cdbcda6774bd26707fd4b08fc615372976565
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-05-25 13:51:23 +0600

    New Slice Added : Overview

--- src/app/store/Store.js
+++ src/app/store/Store.js
@@ -5,9 +5,11 @@ import ProfileSettingData from "../features/ProfileSettingSlice";
 import BillingSettingData from "../features/BillingSettingSlice";
 import LinksSettingSlice from "../features/LinksSettingSlice";
 import FlowControlSlice from "../features/FlowControlSlice";
+import OverviewSlice from "../features/OverviewSlice";
 
 const Store = configureStore({
   reducer: {
+    overviewData: OverviewSlice,
     profileData: ProfileSettingData,
     billingData: BillingSettingData,
     linksData: LinksSettingSlice,
