048c3983edfb49488c44b77d16a4437e86ffb705
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-01 22:54:53 +0600

    Auth Checking Updated

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,32 @@
+// dependencies
+import { useState, useEffect } from "react";
+
 export const useAuth = () => {
-  const auth = true;
+  // state
+  const [auth, setAuth] = useState(true);
+  const reference = {
+    userId: localStorage.getItem("user_id"),
+    refreshToken: localStorage.getItem("refresh"),
+  };
+
+  // get value from server
+  useEffect(() => {
+    const checkFunc = async () => {
+      const response = await fetch(import.meta.env.VITE_ACCOUNT_CHECK, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reference),
+        credentials: "include",
+      });
+
+      const data = await response.json();
+      setAuth(data.success);
+    };
+
+    checkFunc();
+  }, []);
+
   return auth;
 };
