d5ad3941524afff9fbd27bb5a3bf1d54f82353da
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-02 06:55:14 +0600

    LogOut Controller Updated

---
+++ src/handler/AccountLogOut.js
@@ -0,0 +1,33 @@
+// main
+export const AccountLogOut = async () => {
+  // local data
+  const ref = {
+    userId: localStorage.getItem("user_id"),
+    refreshToken: localStorage.getItem("refresh"),
+  };
+
+  try {
+    // request server
+    const response = await fetch(import.meta.env.VITE_ACCOUNT_LOGOUT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(ref),
+      credentials: "include",
+    });
+    // actions
+    const data = await response.json();
+    // response
+    if (data.success) {
+      // clean local storage
+      localStorage.clear();
+      // redirect
+      setTimeout(() => {
+        window.location.href = data.redirectTo;
+      }, 800);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
