544b685244eabd4c1e1e3bfc39451c6eada2d4c7
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-03 08:19:01 +0600

    Unnecessary Slices Removed

--- src/app/store/Store.js
+++ src/app/store/Store.js
@@ -2,9 +2,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 // slices
 import AuthenticationSlice from "../features/AuthenticationSlice";
-import ProfileSettingData from "../features/ProfileSettingSlice";
 import BillingSettingData from "../features/BillingSettingSlice";
-import LinksSettingSlice from "../features/LinksSettingSlice";
 import FlowControlSlice from "../features/FlowControlSlice";
 import OverviewSlice from "../features/OverviewSlice";
 import SignUpSlice from "../features/SignUpSlice";
@@ -14,9 +12,7 @@ const Store = configureStore({
   reducer: {
     authControl: AuthenticationSlice,
     overviewData: OverviewSlice,
-    profileData: ProfileSettingData,
     billingData: BillingSettingData,
-    linksData: LinksSettingSlice,
     flowControl: FlowControlSlice,
     signUpControl: SignUpSlice,
     loginControl: LogInSlice,
