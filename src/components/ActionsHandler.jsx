6c8427b6b369bf937e95c3cd567e95cd5d47a157
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-05 11:59:45 +0600

    Structure Updated : States Managed

--- src/components/ActionsHandler.jsx
+++ src/components/ActionsHandler.jsx
@@ -1,11 +1,17 @@
 // dependencies
 import { useState } from "react";
+import { useSlices } from "../hooks/useSlices";
+// controller
+import { handleTarget } from "../app/features/UrlControllerSlice";
+import { handlePopupView } from "../app/features/FlowControlSlice";
 // icons
 import { Trash2, Copy, Edit2, CheckCheck } from "lucide-react";
 
 // main
 const ActionHandler = ({ info }) => {
   // state
+  const { dispatch: ControlPopup } = useSlices("flowControl");
+  const { dispatch: SetUrlInfo } = useSlices("urlControl");
   const [isCopy, setIsCopy] = useState(false);
 
   // handle Copy
@@ -16,6 +22,13 @@ const ActionHandler = ({ info }) => {
     setTimeout(() => {
       setIsCopy(false);
     }, 1200);
+  };
+
+  // handle delete
+  const handleDelete = async () => {
+    await SetUrlInfo(handleTarget(info));
+    await ControlPopup(handlePopupView("linkDeletion"));
+    console.log("okay");
   };
 
   return (
@@ -36,7 +49,10 @@ const ActionHandler = ({ info }) => {
       <button className="p-1.5 text-slate-400 hover:text-white bg-white/5 hover:bg-white/10 rounded-md transition-colors cursor-pointer">
         <Edit2 className="w-4 h-4" />
       </button>
-      <button className="p-1.5 text-red-400 hover:text-red-300 bg-red-500/10 hover:bg-red-500/20 rounded-md transition-colors cursor-pointer">
+      <button
+        onClick={handleDelete}
+        className="p-1.5 text-red-400 hover:text-red-300 bg-red-500/10 hover:bg-red-500/20 rounded-md transition-colors cursor-pointer"
+      >
         <Trash2 className="w-4 h-4" />
       </button>
     </div>
