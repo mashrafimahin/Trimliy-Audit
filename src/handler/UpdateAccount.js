00a2e1b6e21842bc62ec0be0a86f9af8b8891d75
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-11 16:48:31 +0600

    Structure Updated

---
+++ src/handler/UpdateAccount.js
@@ -0,0 +1,30 @@
+// update user
+export const updateExistingUser = async (userData) => {
+  try {
+    const response = await fetch(
+      "http://localhost:8380/authentication/updateAccount",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...userData }),
+        credentials: "include",
+      },
+    );
+    // data
+    const data = await response.json();
+    // redirection
+    if (data.success) {
+      setTimeout(() => {
+        window.location.href = data.redirectTo;
+      }, 1200);
+    } else {
+      throw Error(data);
+    }
+    return data;
+  } catch (err) {
+    console.log(err?.message);
+    return false;
+  }
+};
