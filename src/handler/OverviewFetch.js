d1c072c5aa460a69eae4e584d1f1db7038892b02
Mashrafi Mahin <mashrafimahin120@gmail.com>
2026-06-14 11:35:32 +0600

    Structure Added : Device Data Merged

--- src/handler/OverviewFetch.js
+++ src/handler/OverviewFetch.js
@@ -1,3 +1,6 @@
+// dependencies
+import { DevicePercentage } from "../utils/DevicePercentage";
+
 // main
 export const OverviewFetch = async () => {
   // user id
@@ -24,10 +27,34 @@ export const OverviewFetch = async () => {
       createdDate: newUrlDate[i],
     }));
 
+    // devices
+    const clickedDevices = [];
+    const targetedDevices = newUrlData.map((item) => item.devices).flat();
+    // add devices
+    clickedDevices.push(
+      {
+        name: "Desktop",
+        percent: DevicePercentage(targetedDevices, "desktop"),
+        color: "bg-purple-500",
+      },
+
+      {
+        name: "Mobile",
+        percent: DevicePercentage(targetedDevices, "mobile"),
+        color: "bg-blue-500",
+      },
+      {
+        name: "Tablet",
+        percent: DevicePercentage(targetedDevices, "tablet"),
+        color: "bg-cyan-500",
+      },
+    );
+
     // original data + modified data
     const newData = {
       ...data,
       urlData: newUrlData,
+      devices: clickedDevices,
     };
 
     // response
